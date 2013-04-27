7e40a8afdef4d173e19b60b36dc60b79b0b6e61b
Sune <Sune@192.168.1.45>
2013-04-28 01:31:55 +0200

    mega upload v2

--- SucklessGame/src/com/suckless/TickControl.java
+++ SucklessGame/src/com/suckless/TickControl.java
@@ -25,30 +25,4 @@ public class TickControl {
 			}
 		}, 0, updateSelector);
 	}
-	
-	public static void main(String[] args) {
-		// Player 1
-		GameObject[] player1 = new GameObject;
-		player1[0] = new Infantry(new Vector2(1.0f,1.0f),1,1,1);
-		player1[1] = new Infantry(new Vector2(1.0f,1.0f),1,1,1);
-		
-		// Player 2
-		GameObject[] player2 = null;
-		player2[0] = new Infantry(new Vector2(1.0f,1.0f),1,1,1);
-		player2[1] = new Infantry(new Vector2(1.0f,1.0f),1,1,1);
-		
-		
-		final GameHandler gameHandling = new GameHandler(player1,player2);
-		new Timer().schedule(new TimerTask() { 
-			public void run()  { 
-				gameHandling.Handle();
-			}
-		}, 0, tickSize);
-		
-		new Timer().schedule(new TimerTask() { 
-			public void run()  { 
-				gameHandling.updateSelection();
-			}
-		}, 0, updateSelector);
-	}
 }
