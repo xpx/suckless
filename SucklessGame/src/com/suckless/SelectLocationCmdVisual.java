61a8af413ffb5e94027ffe25cb4fefbedbbb1b07
Rolf Madsen <rolfrm@gmail.com>
2013-04-28 01:22:52 +0200

    Added graphics for location selector

--- SucklessGame/src/com/suckless/SelectLocationCmdVisual.java
+++ SucklessGame/src/com/suckless/SelectLocationCmdVisual.java
@@ -1,14 +1,23 @@
 package com.suckless;
 
 public class SelectLocationCmdVisual implements CommandVisual {
-
+	int cellX;
+	int cellY;
 	SelectLocationCmdVisual(int cellX, int cellY){
-		
+		this.cellX = cellX;
+		this.cellY = cellY;
 	}
 	
 	@Override
 	public void Draw(RenderState gdx) {
-		// TODO Auto-generated method stub
+		for(int i = 0 ; i < cellX; i++){
+			drawAt(gdx,i,0);
+		}
+		for(int i = 0; i < cellY;i++){
+			drawAt(gdx,cellX,i);
+		}
+	}
+	void drawAt(RenderState gdx, int x, int y){
 		
 	}
 
